4b5665ab7d6c13d046938e65bfbc78fd53785245
Abdul Haqani <ahaqa011@uottawa.ca>
2020-01-11 21:03:06 -0500

    picture

--- app/src/main/java/com/example/ecosnap/MainActivity.kt
+++ app/src/main/java/com/example/ecosnap/MainActivity.kt
@@ -1,9 +1,18 @@
 package com.example.ecosnap
 
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.camerakit.CameraKitView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.security.AccessController.getContext
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var cameraKitView: CameraKitView
@@ -14,6 +23,14 @@ class MainActivity : AppCompatActivity() {
         cameraKitView = findViewById(R.id.camera)
         actionBar?.hide()
         supportActionBar?.hide()
+        captureButton.setOnClickListener(photoOnClickListener)
+
+        facingButton.setOnClickListener{
+            Log.d("Press", "Hello ${facingButton.id}")
+        }
+        flashButton.setOnClickListener{
+            Log.d("Press", "Hello ${flashButton.id}")
+        }
 
     }
 
@@ -40,5 +57,22 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(requestCode: Int, permissions : Array<String>, grantResults : IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         cameraKitView.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
+    // From button OnClickListener
+    private val photoOnClickListener: View.OnClickListener = View.OnClickListener {
+        cameraKitView.captureImage { _, capturedImage ->
+            val savedPhoto =
+                File(this.getExternalFilesDir(null)?.absolutePath, "photo.jpg")
+                Log.d("dir", Environment.getExternalStorageState())
+            try {
+                val outputStream = FileOutputStream(savedPhoto.path)
+                outputStream.write(capturedImage)
+                outputStream.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
     }
 }
