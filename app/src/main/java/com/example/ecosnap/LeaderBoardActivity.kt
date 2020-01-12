74a86392544fefefeec0e17a5e596605199c5781
Abdul Haqani <ahaqa011@uottawa.ca>
2020-01-12 08:24:49 -0500

    push

--- app/src/main/java/com/example/ecosnap/LeaderBoardActivity.kt
+++ app/src/main/java/com/example/ecosnap/LeaderBoardActivity.kt
@@ -2,12 +2,17 @@ package com.example.ecosnap
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.leaderboard.*
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import android.widget.ListView
 
 class LeaderBoardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.leaderboard)
         leaderBoard_outline.apply {
--- app/src/main/java/com/example/ecosnap/LoginActivity.kt
+++ app/src/main/java/com/example/ecosnap/LoginActivity.kt
@@ -1,13 +1,18 @@
 package com.example.ecosnap
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_login.*
 
 
@@ -20,18 +25,11 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        auth = FirebaseAuth.getInstance()
         userName = findViewById(R.id.username)
         password = findViewById(R.id.password)
         signInButton = findViewById(R.id.login)
-    }
-
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-
-
+        FirebaseApp.initializeApp(this)
+        auth = FirebaseAuth.getInstance()
         signInButton.setOnClickListener {
             var emailInput = username.text.toString()
             var passwordInput = password.text.toString()
@@ -42,6 +40,9 @@ class LoginActivity : AppCompatActivity() {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("Message", "Authentication Successful")
                             val user = auth.currentUser
+                            var df = Leaderboard()
+                            Log.d("blablabla", df.toString())
+                            startActivity(Intent(this, MainActivity::class.java))
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.d("Message", "Authentication Failed")
@@ -52,8 +53,27 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+    }
 
+}
+fun Leaderboard() : ArrayList<HashMap<String,  String>>{
+    lateinit var listView: ListView
+    var usersList:ArrayList<HashMap<String,String>> = ArrayList()
 
+    val db = FirebaseFirestore.getInstance()
+    val usersCollection = db.collection("Users")
+    usersCollection.orderBy("points", Query.Direction.DESCENDING).get().addOnSuccessListener { result ->
+        for (document in result) {
+            Log.d("MESSAGE", "${document.id} => ${document.data}")
 
+            usersList.add(document.data as HashMap<String, String>)
+        }
+
+    }
+    return usersList
 
 }
